443d435deffefd3e72a0237e257f363232a5c462
Pat Palmerston <patpalmerston@gmail.com>
2019-08-08 10:13:18 -0700

    testing set up with basic tests

---
+++ test-tutorial/src/calculator.test.js
@@ -0,0 +1,19 @@
+import { add, subtract } from './calculator';
+
+describe('calculator',() => {
+  describe('add()', () => {
+    it('should return the sum of numbers passed in', () => {
+      // expect
+      expect(add(2, 3)).toBe(5);
+      expect(add(2, 2)).toBe(4)
+    })
+  })
+
+  describe('subtract()', () => {
+    it('should return subtracted sum of two numbers passed in', () => {
+      expect(subtract(2, 2)).toBe(0)
+      expect(subtract(5, 2)).toBe(3)
+    })
+  })
+
+})
