18636f129dd1419c8a090bbf3bf117097d08f03e
Pat Palmerston <patpalmerston@gmail.com>
2019-08-07 21:05:05 -0700

    mvp

--- user-onboarding-master/src/UserForm.js
+++ user-onboarding-master/src/UserForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
@@ -6,16 +6,29 @@ import * as Yup from 'yup';
 const UserForm = ({ errors, touched, values, handleSubmit, status }) => {
 	//Name Email Password Terms of Service (checkbox)
 	// A submit button to send our form data to the server
+  const [users, setUser] = useState([])
+  useEffect(() => {
+    if(status) {
+      setUser([...users, status])
+    }
+    // eslint-disable-next-line
+  }, [status])
 
 	return (
 		<div className='user-form'>
 			<Form>
 				<Field type='text' name='name' placeholder='Name' />
-
+          {touched.name && errors.name && (
+            <p className='error'>{errors.name}</p>
+          )}
 				<Field type='email' name='email' placeholder='Email' />
-
+        {touched.email && errors.email && (
+            <p className='error'>{errors.email}</p>
+          )}
 				<Field type='password' name='password' placeholder='Password' />
-
+        {touched.password && errors.password && (
+            <p className='error'>{errors.password}</p>
+          )}
         <Field component="select" className="type-select" name="type">
           <option>Please Choose an Option</option>
           <option value='mage'>Mage</option>
@@ -31,10 +44,42 @@ const UserForm = ({ errors, touched, values, handleSubmit, status }) => {
 
 				<button type="submit">Submit</button>
 			</Form>
+      {users.map(user => (
+        <p key={user.id}>{user.name}</p>
+      ))}
 		</div>
 	);
 };
 
-const FormikUserForm = withFormik({})(UserForm);
+// name email password type terms
+const FormikUserForm = withFormik({
+  mapPropsToStatus({name, email, password, type, terms}) {
+    return {
+      terms: terms || false,
+      type: type || '',
+      password: password || '',
+      email: email || '',
+      name: name || ''
+
+    }
+  },
+
+  ValidationSchema: Yup.object().shape({
+    name: Yup.string().required('add name'),
+    email: Yup.string().required('add email'),
+    password: Yup.string().required('password needed'),
+    type: Yup.string(),
+    terms: Yup.boolean()
+  }),
+
+  handleSubmit(values, {setStatus}){
+    axios 
+      .post(`https://reqres.in/api/users`, values)
+      .then(res => {
+        setStatus(res.data)
+      })
+      .catch(err => console.log(err.response))
+  }
+})(UserForm);
 
 export default FormikUserForm;
